19a347c36226e6c7d591db5760ebb3c633271910
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2022-08-15 10:39:49 -0600

    things

--- script.js
+++ script.js
@@ -3,4 +3,23 @@
 var whitePieces = 0;
 var blackPieces = 0;
 
+//Turn grid into a 2D array
+var board = new Array(8);
+for (var i = 0; i < board.length; i++) {
+    board[i] = new Array(8);
+}
+
+var x = 1;
+var y = 1;
+//Fill 2D array with Grid Elements
+for (var i = 0; i < 8; i++) {
+    for (var j = 0; j < 8; j++) {
+        board[i][j] = document.getElementById(`btn${x},${y}`);
+        x++;
+    }
+    y++;
+}
+
+
+
 
