65143664c86bf2edba4248b209e6e15d6ec52300
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2022-08-18 10:16:22 -0600

    broken

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 var whitePieces = 0;
 var blackPieces = 0;
 var board = new Array(8);
-var currentPlayer = 0;
+var currentPlayer = 1;
 var player1Token = "";
 var player2Token = "";
 
-function buildBoard(){
+function buildBoard() {
     //Turn grid into a 2D array
     for (var i = 0; i < board.length; i++) {
         board[i] = new Array(8);
@@ -16,125 +16,221 @@ function buildBoard(){
     //Fill 2D array with Grid Elements
     for (var i = 0; i < 8; i++) {
         for (var j = 0; j < 8; j++) {
-            board[i][j] = document.getElementById(`btn${i+1},${j+1}`);
+            board[i][j] = document.getElementById(`btn${i},${j}`);
         }
     }
+    board[3][3].getElementsByTagName("img")[0].src = player1Token;
+    board[4][4].getElementsByTagName("img")[0].src = player1Token;
+    board[3][4].getElementsByTagName("img")[0].src = player2Token;
+    board[4][3].getElementsByTagName("img")[0].src = player2Token;
+
 }
 
-function setPlayerToken(clickID){
+function setPlayerToken(clickID) {
     console.log(clickID);
-    if(currentPlayer == 0){
+    if (currentPlayer == 0) {
         player1Token = document.getElementById(clickID).getElementsByTagName("img")[0].src;
         currentPlayer = 1;
         console.log(player1Token);
     }
-    else{
+    else {
         player2Token = document.getElementById(clickID).getElementsByTagName("img")[0].src;
         currentPlayer = 0;
         console.log(player2Token);
     }
 }
 
-function flip(){
+function flip(clickID) {
     //When btn is clicked
     //Check if any adjacent pieces are the opposite color
     //IF adjacent pieces are the opposite color,
-        //Check the next piece over until they find the same color
-        //Save each btnID
-        //Change all btnID to the current color
-    //ELSE don't
-}
+    var xyPoints = clickID.substring(3);
 
-function checkHorizontal(x, y){
-    var validRow = false;
-    //If current player is 1
-    if(currentPlayer == 1){
-        //Check black pieces with a white piece next to it
-        for (var i = x; i < 8; i++) {
-            for (var j = y; j < 8; j++) {
-                if(board[i][j].innerHTML == "Black"){
-                    if(board[i+1][j].innerHTML == "White" || board[i-1][j].innerHTML == "White"){
-                        validRow = true;
-                    }
-                }
-                
-            }
-        }
-    }
-    else if(currentPlayer == 0){
-        //Check white pieces with a black piece next to it
-        for (var i = 0; i < 8; i++) {
-            for (var j = 0; j < 8; j++) {
-                if(board[i][j].innerHTML == "White"){
-                    if(board[i+1][j].innerHTML == "Black" || board[i-1][j].innerHTML == "Black"){
-                        validRow = true;
-                    }
+    if (checkHorizontal(xyPoints[0], xyPoints[2])) {
+        if (currentPlayer == 0) {
+            document.getElementById(`btn${xyPoints[0]},${xyPoints[2]}`).getElementsByTagName("img")[0].src = player1Token;
+            for (var i = xyPoints[2]; i < 8; i++) {
+                if (board[xyPoints[0]][i].getElementsByTagName("img")[0].src === player2Token) {
+                    board[xyPoints[0]][i+1].getElementsByTagName("img")[0].src = player1Token;
                 }
             }
+            currentPlayer = 1;
+        }
+        else {
+            document.getElementById(`btn${xyPoints[0]},${xyPoints[2]}`).getElementsByTagName("img")[0].src = player2Token;
+            for (var i = xyPoints[0]; i < 8; i++) {
+                if ([xyPoints[0]][i].getElementsByTagName("img")[0].src === player1Token) {
+                    board[xyPoints[0]][i-1].getElementsByTagName("img")[0].src = player2Token;
+                }
+            }
+            currentPlayer = 0;
         }
     }
-return validRow;
-}
-function checkVertical(x, y){
-    var validColumn = false;
+        
+        console.log("Vertical Check: " + checkVertical(xyPoints[0], xyPoints[2]))
+        if (checkVertical(xyPoints[0], xyPoints[2])) {
+            if (currentPlayer == 0) {
+                document.getElementById(`btn${xyPoints[0]},${xyPoints[2]}`).getElementsByTagName("img")[0].src = player1Token;
+                for (var i = xyPoints[0]; i < 8; i++) {
+                    if (board[i][xyPoints[2]].getElementsByTagName("img")[0].src === player2Token) {
+                        board[i + 1][xyPoints[2]].getElementsByTagName("img")[0].src = player1Token;
+                    }
+                }
+                currentPlayer = 1;
+            }
+            else {
+                document.getElementById(`btn${xyPoints[0]},${xyPoints[2]}`).getElementsByTagName("img")[0].src = player2Token;
+                for (var i = xyPoints[0]; i < 8; i++) {
+                    if (board[i][xyPoints[2]].getElementsByTagName("img")[0].src === player1Token) {
+                        board[i + 1][xyPoints[2]].getElementsByTagName("img")[0].src = player2Token;
+                    }
+                }
+                currentPlayer = 0;
+            }
+        }
+
+        // console.log("Diagonal Check: " + checkDiagonal(xyPoints[0], xyPoints[2]))
+        // if (checkDiagonal(xyPoints[0], xyPoints[2])) {
+        //     for (var i = xyPoints[0]; i < 8; i++) {
+        //         for (var j = xyPoints[2]; j < 8; j++) {
+        //             if (board[i + 1][j + 1].getElementsByTagName("img")[0].src == player2Token) {
+        //                 board[i + 1][j + 1].getElementsByTagName("img")[0].src = player1Token;
+        //             }
+        //             if (board[i - 1][j - 1].getElementsByTagName("img")[0].src == player2Token) {
+        //                 board[i - 1][j - 1].getElementsByTagName("img")[0].src = player1Token;
+        //             }
+        //             if (board[i + 1][j - 1].getElementsByTagName("img")[0].src == player2Token) {
+        //                 board[i + 1][j - 1].getElementsByTagName("img")[0].src = player1Token;
+        //             }
+        //             if (board[i - 1][j + 1].getElementsByTagName("img")[0].src == player2Token) {
+        //                 board[i - 1][j + 1].getElementsByTagName("img")[0].src = player1Token;
+        //             }
+        //         }
+        //     }
+        // }
+        
+    }
+
+    function checkHorizontal(x, y) {
+        var validRow = false;
         //If current player is 1
-        if(currentPlayer == 1){
+        if (currentPlayer == 0) {
+            //Check black pieces with a white piece next to it
+            for (var i = y; i < 8; i++) {
+                if (board[x][i].getElementsByTagName("img")[0].src === player2Token) {
+
+                    if (board[x][i + 1].getElementsByTagName("img")[0].src != null && board[x][i + 1].getElementsByTagName("img")[0].src === player1Token) {
+                        validRow = true;
+                    }
+                }
+            }
+
+            for (var i = y; i > 0; i -= 1) {
+                if (board[x][i].getElementsByTagName("img")[0].src === player2Token) {
+
+                    if (board[x][i - 1].getElementsByTagName("img")[0].src != null && board[x][i - 1].getElementsByTagName("img")[0].src === player1Token) {
+                        validRow = true;
+                    }
+                }
+            }
+        }
+        else if (currentPlayer == 1) {
+            //Check white pieces with a black piece next to it
+            for (var i = y; i < 8; i++) {
+                if (board[x][i].getElementsByTagName("img")[0].src === player1Token) {
+
+                    if (board[x][i + 1].getElementsByTagName("img")[0].src != null && board[x][i + 1].getElementsByTagName("img")[0].src === player2Token) {
+                        validRow = true;
+                    }
+                }
+            }
+
+            for (var i = y; i > 0; i -= 1) {
+                if (board[x][i].getElementsByTagName("img")[0].src === player1Token) {
+
+                    if (board[x][i - 1].getElementsByTagName("img")[0].src != null && board[x][i - 1].getElementsByTagName("img")[0].src === player2Token) {
+                        validRow = true;
+                    }
+                }
+            }
+        }
+        return validRow;
+    }
+    function checkVertical(x, y) {
+        var validColumn = false;
+        //If current player is 1
+        if (currentPlayer == 0) {
             //Check black pieces with a white piece next to it
             for (var i = x; i < 8; i++) {
-                for (var j = y; j < 8; j++) {
-                    if(board[i][j].innerHTML == "Black"){
-                        if(board[i][j+1].innerHTML == "White" || board[i][j-1].innerHTML == "White"){
-                            validColumn = true;
-                        }
+                if (board[i][y].getElementsByTagName("img")[0].src === player2Token) {
+
+                    if (board[i + 1][y].getElementsByTagName("img")[0].src != null && board[i + 1][y].getElementsByTagName("img")[0].src === player1Token) {
+                        validRow = true;
                     }
-                    
+                }
+            }
+
+            for (var i = x; i > 0; i -= 1) {
+                if (board[i][y].getElementsByTagName("img")[0].src === player2Token) {
+
+                    if (board[i + 1][y].getElementsByTagName("img")[0].src != null && board[i - 1][y].getElementsByTagName("img")[0].src === player1Token) {
+                        validRow = true;
+                    }
                 }
             }
         }
-        else if(currentPlayer == 0){
+        else if (currentPlayer == 1) {
             //Check white pieces with a black piece next to it
-            for (var i = 0; i < 8; i++) {
-                for (var j = 0; j < 8; j++) {
-                    if(board[i][j].innerHTML == "White"){
-                        if(board[i][j+1].innerHTML == "Black" || board[i][j-1].innerHTML == "Black"){
-                            validColumn = true;
-                        }
+            for (var i = x; i < 8; i++) {
+                if (board[i][y].getElementsByTagName("img")[0].src === player1Token) {
+
+                    if (board[i + 1][y].getElementsByTagName("img")[0].src != null && board[i + 1][y].getElementsByTagName("img")[0].src === player2Token) {
+                        validRow = true;
+                    }
+                }
+            }
+
+            for (var i = x; i > 0; i -= 1) {
+                if (board[i][y].getElementsByTagName("img")[0].src === player1Token) {
+
+                    if (board[i + 1][y].getElementsByTagName("img")[0].src != null && board[i - 1][y].getElementsByTagName("img")[0].src === player2Token) {
+                        validRow = true;
                     }
                 }
             }
         }
-    return validColumn;
-}
-function checkDiagonal(x, y){
-    var validDiagonal = false;
-    //If current player is 1
-    if(currentPlayer == 1){
-        //Check black pieces with a white piece next to it
-        for (var i = x; i < 8; i++) {
-            for (var j = y; j < 8; j++) {
-                if(board[i][j].innerHTML == "Black"){
-                    if(board[i+1][j+1].innerHTML == "White" || board[i-1][j-1].innerHTML == "White" || board[i+1][j-1].innerHTML == "White" || board[i-1][j+1].innerHTML == "White"){
-                        validDiagonal = true;
-                    }
-                }
-                
-            }
-        }
+        return validColumn;
     }
-    else if(currentPlayer == 0){
-        //Check white pieces with a black piece next to it
-        for (var i = 0; i < 8; i++) {
-            for (var j = 0; j < 8; j++) {
-                if(board[i][j].innerHTML == "White"){
-                    if(board[i+1][j+1].innerHTML == "Black" || board[i-1][j-1].innerHTML == "Black" || board[i+1][j-1].innerHTML == "Black" || board[i-1][j+1].innerHTML == "Black"){
-                        validDiagonal = true;
-                    }
-                }
-            }
-        }
-    }
-    return validDiagonal;
-}
+// function checkDiagonal(x, y) {
+//     var validDiagonal = false;
+//     //If current player is 1
+//     if (currentPlayer == 0) {
+//         //Check black pieces with a white piece next to it
+//         for (var i = x; i < 8; i++) {
+//             for (var j = y; j < 8; j++) {
+//                 if (board[i][j].getElementsByTagName("img")[0].src == player1Token) {
+//                     if (board[i + 1][j + 1].getElementsByTagName("img")[0].src == player2Token || board[i - 1][j - 1].getElementsByTagName("img")[0].src == player2Token || board[i + 1][j - 1].getElementsByTagName("img")[0].src == player2Token || board[i - 1][j + 1].getElementsByTagName("img")[0].src == player2Token) {
+//                         validDiagonal = true;
+//                     }
+//                 }
+
+//             }
+//         }
+//     }
+//     else if (currentPlayer == 1) {
+//         //Check white pieces with a black piece next to it
+//         for (var i = 0; i < 8; i++) {
+//             for (var j = 0; j < 8; j++) {
+//                 if (board[i][j].getElementsByTagName("img")[0].src == player2Token) {
+//                     if (board[i + 1][j + 1].getElementsByTagName("img")[0].src == player1Token || board[i - 1][j - 1].getElementsByTagName("img")[0].src == player1Token || board[i + 1][j - 1].getElementsByTagName("img")[0].src == player1Token || board[i - 1][j + 1].getElementsByTagName("img")[0].src == player1Token) {
+//                         validDiagonal = true;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return validDiagonal;
+// }
 
 
 
